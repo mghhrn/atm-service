0d4e56168234753f4cebf7cb99b26e761baf4ed8
Mahdi Ghaharian <m.ghaharian@gmail.com>
2021-11-04 02:52:55 +0330

    base commit

---
+++ src/test/java/ir/mghhrn/atm/AtmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mghhrn.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
