7e4a4ded6e601cb318d5547f276874d2aa6b8399
Mahdi Ghaharian <m.ghaharian@gmail.com>
2021-11-04 10:22:44 +0330

    added log configuration and some logging

--- src/main/java/ir/mghhrn/atm/controller/BankOperationController.java
+++ src/main/java/ir/mghhrn/atm/controller/BankOperationController.java
@@ -2,6 +2,8 @@ package ir.mghhrn.atm.controller;
 
 import ir.mghhrn.atm.bank.BankServiceClient;
 import ir.mghhrn.atm.bank.dto.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/bank-operation")
 public class BankOperationController {
 
+    private static final Logger logger = LogManager.getLogger(BankOperationController.class);
     private static final String AUTHENTICATION_TOKEN_HEADER = "AUTHENTICATION_TOKEN";
 
     private final BankServiceClient bankServiceClient;
@@ -22,6 +25,7 @@ public class BankOperationController {
     public ResponseEntity<CardVerificationResponseDto> verifyCard(
             @RequestBody CardVerificationRequestDto requestDto) {
         CardVerificationResponseDto responseDto = bankServiceClient.verifyCard(requestDto);
+        logger.info("successfully verified card {}", requestDto.getCardNumber());
         return ResponseEntity.ok(responseDto);
     }
 
@@ -29,6 +33,7 @@ public class BankOperationController {
     public ResponseEntity<AuthenticationResponseDto> authenticateCard(
             @RequestBody AuthenticationRequestDto requestDto) {
         AuthenticationResponseDto responseDto = bankServiceClient.authenticateCard(requestDto);
+        logger.info("successfully authenticated card {}", requestDto.getCardNumber());
         return ResponseEntity.ok(responseDto);
     }
 
@@ -36,6 +41,7 @@ public class BankOperationController {
     public ResponseEntity<Void> closeSession(
             @RequestHeader(AUTHENTICATION_TOKEN_HEADER) String authenticationToken) {
         bankServiceClient.closeSession(authenticationToken);
+        logger.info("successfully closed session for token {}", authenticationToken);
         return ResponseEntity.ok().build();
     }
 
@@ -43,6 +49,7 @@ public class BankOperationController {
     public ResponseEntity<CheckBalanceResponseDto> getBalance(
             @RequestHeader(AUTHENTICATION_TOKEN_HEADER) String authenticationToken) {
         CheckBalanceResponseDto responseDto = bankServiceClient.getBalance(authenticationToken);
+        logger.info("successfully got balance");
         return ResponseEntity.ok(responseDto);
     }
 
@@ -51,6 +58,7 @@ public class BankOperationController {
             @RequestBody WithdrawRequestDto requestDto,
             @RequestHeader(AUTHENTICATION_TOKEN_HEADER) String authenticationToken) {
         AccountTransactionResponseDto responseDto = bankServiceClient.withdrawMoney(authenticationToken, requestDto);
+        logger.info("successfully withdrew amount of {} from account {}", requestDto.getAmount(), responseDto.getAccountId());
         return ResponseEntity.ok(responseDto);
     }
 
@@ -59,6 +67,7 @@ public class BankOperationController {
             @RequestBody DepositRequestDto requestDto,
             @RequestHeader(AUTHENTICATION_TOKEN_HEADER) String authenticationToken) {
         AccountTransactionResponseDto responseDto = bankServiceClient.depositMoney(authenticationToken, requestDto);
+        logger.info("successfully deposited amount of {} to account {}", requestDto.getDepositAmount(), responseDto.getAccountId());
         return ResponseEntity.ok(responseDto);
     }
 
@@ -67,6 +76,7 @@ public class BankOperationController {
             @RequestBody AuthenticationModeRequestDto requestDto,
             @RequestHeader(AUTHENTICATION_TOKEN_HEADER) String authenticationToken) {
         bankServiceClient.changeAuthenticationMode(authenticationToken, requestDto);
+        logger.info("successfully changed authentication mode");
         return ResponseEntity.ok().build();
     }
 }
